e4a60130de17b06cc3b4c297ccb20da54fc1767e
Huy Vo <huythvo@users.noreply.github.com>
2016-08-17 21:58:26 +0700

    Update gcd.py

--- algorithmic-toolbox/1-intro-starter-files/gcd.py
+++ algorithmic-toolbox/1-intro-starter-files/gcd.py
@@ -2,14 +2,11 @@
 import sys
 
 def gcd(a, b):
-    if a > b:
-        while b != 0:
-            a, b = b, a%b
-        return a
-    else:
-        while a != 0:
-            b, a = a, b%a
-        return b
+    if b < a:
+        a, b = b, a
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 if __name__ == "__main__":
     input = sys.stdin.read()
