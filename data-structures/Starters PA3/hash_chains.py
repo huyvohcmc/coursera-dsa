af4f51325d74448f25fec0592e85cce4e02f6d09
Huy Vo <huyvohcmc@gmail.com>
2016-10-30 00:23:32 +0700

    added week 4

--- data-structures/Starters PA3/hash_chains.py
+++ data-structures/Starters PA3/hash_chains.py
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[]] * self.bucket_count
 
     def _hash_func(self, s):
         ans = 0
@@ -36,22 +36,17 @@ class QueryProcessor:
 
     def process_query(self, query):
         if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(self.elems[query.ind])
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+            ind = self._hash_func(query.s)
             if query.type == 'find':
-                self.write_search_result(ind != -1)
+                self.write_search_result(query.s in self.elems[ind])
             elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+                if query.s not in self.elems[ind]:
+                    self.elems[ind] = [query.s] + self.elems[ind]
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                if query.s in self.elems[ind]:
+                    self.elems[ind] = [s for s in self.elems[ind] if s != query.s]
 
     def process_queries(self):
         n = int(input())
--- data-structures/Starters PA3/hash_substring.py
+++ data-structures/Starters PA3/hash_substring.py
@@ -6,13 +6,36 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+def PolyHash(s, prime, multiplier):
+    hash = 0
+    for c in reversed(s):
+        hash = (hash * multiplier + ord(c)) % prime
+    return hash
 
+def PrecomputeHashes(text, len_pattern, prime, multiplier):
+    H = [None] * (len(text) - len_pattern + 1)
+    S = text[len(text) - len_pattern:]
+    H[len(text) - len_pattern] = PolyHash(S, prime, multiplier)
+    y = 1
+    for i in range(len_pattern):
+        y = (y * multiplier) % prime
+    for i in range(len(text) - len_pattern - 1, -1, -1):
+        H[i] = (multiplier * H[i+1] + ord(text[i]) - y * ord(text[i + len_pattern])) % prime
+    return H
+ 
+def get_occurrences(pattern, text):
+    result = []
+    prime = 1610612741
+    multiplier = 263
+    p_hash = PolyHash(pattern, prime, multiplier)
+    H = PrecomputeHashes(text, len(pattern), prime, multiplier)
+
+    for i in range(len(text) - len(pattern) + 1):
+        if (p_hash == H[i]) and (text[i:i + len(pattern)] == pattern):
+            result.append(i)
+
+    return result
+    
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
