2c58fb0658624c6cc5f80b6cec0ddbb61f57b633
Huy Vo <huyvohcmc@gmail.com>
2017-03-09 00:06:34 +0700

    Done implement TrieMatching

--- algorithms-on-strings/Suffix Trees/trie_matching.py
+++ algorithms-on-strings/Suffix Trees/trie_matching.py
@@ -1,25 +1,58 @@
 # python3
 import sys
 
-NA = -1
+def build_trie(patterns):
+    tree = dict()
+    tree[0] = {}
+    index = 1
 
-class Node:
-	def __init__ (self):
-		self.next = [NA] * 4
+    for pattern in patterns:
+        current = tree[0]
+        for letter in pattern:
+            if letter in current.keys():
+                current = tree[current[letter]]
+            else:
+                current[letter] = index
+                tree[index] = {}
+                current = tree[index]
+                index = index + 1
+    return tree
 
 def solve (text, n, patterns):
-	result = []
+    result = []
+    trie = build_trie(patterns)
 
-	// write your code here
+    n = len(text)
+    for i in range(n):
+        if prefix_trie_matching(text[i:], trie):
+            result.append(i)
 
-	return result
+    return result
 
-text = sys.stdin.readline ().strip ()
-n = int (sys.stdin.readline ().strip ())
-patterns = []
-for i in range (n):
-	patterns += [sys.stdin.readline ().strip ()]
+def prefix_trie_matching(text, trie):
+    idx = 0
+    symbol = text[idx]
+    current = trie[0]
+    
+    while True:
+        if not current:
+            return True
+        elif symbol in current.keys():
+            current = trie[current[symbol]]
+            idx = idx + 1
+            if idx < len(text):
+                symbol = text[idx]
+            else:
+                symbol = '@'
+        else:
+            return False
 
-ans = solve (text, n, patterns)
+if __name__ == "__main__":
+    text = sys.stdin.readline().strip()
+    n = int(sys.stdin.readline().strip())
+    patterns = []
+    for i in range(n):
+        patterns += [sys.stdin.readline().strip()]
 
-sys.stdout.write (' '.join (map (str, ans)) + '\n')
+    ans = solve(text, n, patterns)
+    sys.stdout.write(' '.join (map(str, ans)) + '\n')
