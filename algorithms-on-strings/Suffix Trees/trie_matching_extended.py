d4712e4e23dbbb168d33daa2aea88eee6b77a4f9
Huy Vo <huyvohcmc@gmail.com>
2017-03-11 21:01:52 +0700

    Done Extend TrieMatching

--- algorithms-on-strings/Suffix Trees/trie_matching_extended.py
+++ algorithms-on-strings/Suffix Trees/trie_matching_extended.py
@@ -1,26 +1,71 @@
 # python3
 import sys
 
-NA = -1
+def build_trie(patterns):
+    tree = dict()
+    tree[0] = {}
+    index = 1
 
-class Node:
-	def __init__ (self):
-		self.next = [NA] * 4
-		self.patternEnd = False
+    for pattern in patterns:
+        current = tree[0]
+        for letter in pattern:
+            if letter in current:
+                current = tree[current[letter]]
+            else:
+                current[letter] = index
+                tree[index] = {}
+                current = tree[index]
+                index = index + 1
+        current['$'] = {}
+    return tree
+
+def prefix_trie_matching(text, trie, external_idx):
+    idx = 0
+    symbol = text[idx]
+    current = trie[0]
+    res = -1
+    
+    while True:
+        if not current:
+            return res
+        if '$' in current:
+            return res
+        if symbol in current:
+            current = trie[current[symbol]]
+            res = external_idx
+            idx += 1
+            if idx < len(text):
+                symbol = text[idx]
+            else:
+                if '$' in current:
+                    return res
+                else:
+                    symbol = '@'
+                    res = -1
+        else:
+            if '$' in current:
+                return res
+            return -1
 
 def solve (text, n, patterns):
-	result = []
+    result = set()
+    trie = build_trie(patterns)
+    # print(trie)
+    n = len(text)
+    for i in range(n):
+        value = prefix_trie_matching(text[i:], trie, i)
+        if value != -1:
+            result.add(value)
 
-	// write your code here
+    return sorted(list(result))
 
-	return result
 
-text = sys.stdin.readline ().strip ()
-n = int (sys.stdin.readline ().strip ())
+text = sys.stdin.readline().strip()
+n = int(sys.stdin.readline().strip())
 patterns = []
-for i in range (n):
-	patterns += [sys.stdin.readline ().strip ()]
+for i in range(n):
+	patterns += [sys.stdin.readline().strip()]
 
-ans = solve (text, n, patterns)
+ans = solve(text, n, patterns)
 
-sys.stdout.write (' '.join (map (str, ans)) + '\n')
+sys.stdout.write(' '.join(map(str, ans)) + '\n')
