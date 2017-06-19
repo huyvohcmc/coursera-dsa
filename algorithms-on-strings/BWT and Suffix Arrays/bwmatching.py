3bb6ec1ec4e1d5e2de0f05b7659abb0f7eb2bb64
Huy Vo <huyvohcmc@gmail.com>
2017-06-19 21:06:03 +0700

    Done week 2

--- algorithms-on-strings/BWT and Suffix Arrays/bwmatching.py
+++ algorithms-on-strings/BWT and Suffix Arrays/bwmatching.py
@@ -3,41 +3,48 @@ import sys
 
 
 def PreprocessBWT(bwt):
-  """
-  Preprocess the Burrows-Wheeler Transform bwt of some text
-  and compute as a result:
-    * starts - for each character C in bwt, starts[C] is the first position 
-        of this character in the sorted array of 
-        all characters of the text.
-    * occ_count_before - for each character C in bwt and each position P in bwt,
-        occ_count_before[C][P] is the number of occurrences of character C in bwt
-        from position 0 to position P inclusive.
-  """
-  # Implement this function yourself
-  pass
+    sorted_bwt = ''.join(sorted(bwt))
+    char_set = set(sorted_bwt)
+    starts = {x: sorted_bwt.find(x) for x in char_set}
+
+    occ_count_before = {}
+    
+    for x in char_set:
+        lst = [0]
+        counter = 0
+        for i in range(len(bwt)):
+            if bwt[i] == x:
+                counter += 1
+            lst.append(counter)
+        occ_count_before[x] = lst 
+
+    return starts, occ_count_before
 
 
 def CountOccurrences(pattern, bwt, starts, occ_counts_before):
-  """
-  Compute the number of occurrences of string pattern in the text
-  given only Burrows-Wheeler Transform bwt of the text and additional
-  information we get from the preprocessing stage - starts and occ_counts_before.
-  """
-  # Implement this function yourself
-  return 0
-     
+    top, bottom = 0, len(bwt) - 1
+
+    while top <= bottom:
+        if pattern:
+            symbol = pattern[-1]
+            pattern = pattern[:-1]
+            if symbol in bwt[top:bottom+1]:
+                first_occurence = starts[symbol]
+                top = first_occurence + occ_counts_before[symbol][top]
+                bottom = first_occurence + occ_counts_before[symbol][bottom+1] - 1
+            else:
+                return 0
+        else:
+            return bottom - top + 1
 
 
 if __name__ == '__main__':
-  bwt = sys.stdin.readline().strip()
-  pattern_count = int(sys.stdin.readline().strip())
-  patterns = sys.stdin.readline().strip().split()
-  # Preprocess the BWT once to get starts and occ_count_before.
-  # For each pattern, we will then use these precomputed values and
-  # spend only O(|pattern|) to find all occurrences of the pattern
-  # in the text instead of O(|pattern| + |text|).  
-  starts, occ_counts_before = PreprocessBWT(bwt)
-  occurrence_counts = []
-  for pattern in patterns:
-    occurrence_counts.append(CountOccurrences(pattern, bwt, starts, occ_counts_before))
-  print(' '.join(map(str, occurrence_counts)))
+    bwt = sys.stdin.readline().strip()
+    pattern_count = int(sys.stdin.readline().strip())
+    patterns = sys.stdin.readline().strip().split()
+
+    starts, occ_counts_before = PreprocessBWT(bwt)
+    occurrence_counts = []
+    for pattern in patterns:
+        occurrence_counts.append(CountOccurrences(pattern, bwt, starts, occ_counts_before))
+    print(' '.join(map(str, occurrence_counts)))
--- algorithms-on-strings/BWT and Suffix Arrays/suffix_array.py
+++ algorithms-on-strings/BWT and Suffix Arrays/suffix_array.py
@@ -3,18 +3,21 @@ import sys
 
 
 def build_suffix_array(text):
-  """
-  Build suffix array of the string text and
-  return a list result of the same length as the text
-  such that the value result[i] is the index (0-based)
-  in text where the i-th lexicographically smallest
-  suffix of text starts.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+    result = []
+    suffixes = []
+
+    for i in range(len(text)):
+        suffixes.append((i, text[i:]))
+
+    suffixes = sorted(suffixes, key=lambda t: t[1])
+
+    for i in range(len(suffixes)):
+        result.append(suffixes[i][0])
+
+    return result
+    
 
 
 if __name__ == '__main__':
-  text = sys.stdin.readline().strip()
-  print(" ".join(map(str, build_suffix_array(text))))
+    text = sys.stdin.readline().strip()
+    print(" ".join(map(str, build_suffix_array(text))))
