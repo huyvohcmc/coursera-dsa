618a26ca22b8d8b7092598059b2a9cf5fb1b466b
Huy Vo <huyvohcmc@gmail.com>
2017-06-29 20:37:05 +0700

    Done week 3

--- algorithms-on-strings/Algorithmic Challenges/kmp.py
+++ algorithms-on-strings/Algorithmic Challenges/kmp.py
@@ -2,20 +2,37 @@
 import sys
 
 
+def compute_prefix_function(pattern):
+    s = [0] * len(pattern)
+    border = 0
+    
+    for i in range(1, len(pattern)):
+        while (border > 0) and (pattern[i] != pattern[border]):
+            border = s[border - 1]
+        if pattern[i] == pattern[border]:
+            border = border + 1
+        else:
+            border = 0
+        s[i] = border
+    
+    return s
+
 def find_pattern(pattern, text):
-  """
-  Find all the occurrences of the pattern in the text
-  and return a list of all positions in the text
-  where the pattern starts in the text.
-  """
-  result = []
-  # Implement this function yourself
-  return result
+    S = pattern + '$' + text
+    s = compute_prefix_function(S)
+    result = []
+
+    p = len(pattern)
+
+    for i in range(p + 1, len(S)):
+        if s[i] == p:
+            result.append(i - 2 * p)
+
+    return result
 
 
 if __name__ == '__main__':
-  pattern = sys.stdin.readline().strip()
-  text = sys.stdin.readline().strip()
-  result = find_pattern(pattern, text)
-  print(" ".join(map(str, result)))
-
+    pattern = sys.stdin.readline().strip()
+    text = sys.stdin.readline().strip()
+    result = find_pattern(pattern, text)
+    print(" ".join(map(str, result)))
